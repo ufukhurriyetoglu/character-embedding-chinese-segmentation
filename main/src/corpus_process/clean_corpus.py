9c9748404172cb76bc2da48f92b53fc61a67d7f0
Peiyu Liao <u3512485@connect.hku.hk>
2017-10-26 17:37:30 +0800

    Remove argparse redundant components; refine corpus cleaning

--- main/src/corpus_process/clean_corpus.py
+++ main/src/corpus_process/clean_corpus.py
@@ -4,15 +4,23 @@ import re
 
 # Parse args
 parser = argparse.ArgumentParser(description='Clean Chinese corpus')
-parser.add_argument('filename', metavar='F', type=str, nargs=1, 
+parser.add_argument('filename', metavar='F', type=str,
                     help='file to be cleaned')
 
 args = parser.parse_args()
 
 
 # Clean corpus
-with open(args.filename[0]) as f:
+with open(args.filename) as f:
     for line in f:
-        for c in re.findall(r'[\u4e00-\u9fff|\s]+', line):
+        # punctuation at start of line (indicate seg)
+        print('p', end='')
+
+        # main content
+        line_clean = re.findall(r'[\u4e00-\u9fff]+', line)
+        line_rm_space = 'p'.join(line_clean) 
+        for c in line_rm_space:
             print(c, end='')
-        print('')
+        
+        # punctuation at end of line (indicate seg)
+        print('p')
--- main/src/corpus_process/seg_corpus.py
+++ main/src/corpus_process/seg_corpus.py
@@ -3,19 +3,19 @@ import argparse
 
 # Parse args
 parser = argparse.ArgumentParser(description='Segment Chinese corpus into characters')
-parser.add_argument('input', metavar='FI', type=str, nargs=1, 
+parser.add_argument('input', metavar='FI', type=str,
                     help='file to be segmented')
-parser.add_argument('--output', '-o', metavar='FO', type=str, nargs=1,
+parser.add_argument('--output', '-o', metavar='FO', type=str,
                     help='output file name')
 
 args = parser.parse_args()
 
 
 # Segment corpus, each line contains a phrase with characters separated by space
-with open(args.input[0], 'r') as fin:
-    with open(args.output[0], 'w') as fout:
+with open(args.input, 'r') as fin:
+    with open(args.output, 'w') as fout:
         for line in fin:
-            phrases = line.split()
+            phrases = line.split('p')[1:-1]
             for phrase in phrases:
                 chars = ' '.join(list(phrase))
 
