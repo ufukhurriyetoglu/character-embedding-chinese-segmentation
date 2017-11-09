2383beb1dedda78b3ef40b089e3e5380be260a0c
Peiyu Liao <u3512485@connect.hku.hk>
2017-11-09 22:05:28 +0800

    Update run segment script

--- main/run_segment.sh
+++ main/run_segment.sh
@@ -5,8 +5,9 @@ k=50
 vd=250
 algo="k_means"
 seg_algo="naive_threshold"
+dataset="as"
 
-while getopts "td:k:a:s:" opt; do
+while getopts "td:k:a:s:c:" opt; do
     case "$opt" in
         t)  pre="small_"
             ;;
@@ -18,15 +19,17 @@ while getopts "td:k:a:s:" opt; do
             ;;
         s)  seg_algo=${OPTARG}
             ;;
+        c)  dataset=${OPTARG}
+            ;;
     esac
 done
 
-if [ ! -f output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg.txt ]; then
+if [ ! -f output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg_${dataset}.txt ]; then
     if [ ! -d output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo} ]; then
         mkdir output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}
     fi
 
-    touch output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg.txt
+    touch output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg_${dataset}.txt
 fi
 
-python3 -m src.segment.${seg_algo} corpus/segmentation/${pre}test.txt --model output/d_${vd}_k_${k}_${algo}/char2vec.bin --clusters output/d_${vd}_k_${k}_${algo}/char2clusters.bin --comatrix output/d_${vd}_k_${k}_${algo}/cooccur_matrix.bin --output output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg.txt
+python3 -m src.segment.${seg_algo} corpus/segmentation/${pre}test_${dataset}.txt --model output/d_${vd}_k_${k}_${algo}/char2vec.bin --clusters output/d_${vd}_k_${k}_${algo}/char2clusters.bin --comatrix output/d_${vd}_k_${k}_${algo}/cooccur_matrix.bin --output output/segmentation/${seg_algo}/d_${vd}_k_${k}_${algo}/${pre}seg_${dataset}.txt
